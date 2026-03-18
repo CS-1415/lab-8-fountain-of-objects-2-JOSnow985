2c01a75008ee0a59cf8b2a6515ac5606c83a885d
JOSnow985 <jaden.olvera@students.snow.edu>
2026-03-17 23:33:37 -0600

    Build and Test files

--- Lab08.Tests/obj/Debug/net8.0/Lab08.Tests.AssemblyInfo.cs
+++ Lab08.Tests/obj/Debug/net8.0/Lab08.Tests.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Lab08.Tests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1be86c1561eec123f4da842a68e5d254f193a31e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ccc55d55f6363cbd7b8ea423abadfa72cef329e8")]
 [assembly: System.Reflection.AssemblyProductAttribute("Lab08.Tests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Lab08.Tests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
